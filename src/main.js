eaebeb6dec51a3a93fff09c87030aa41e58d14a4
chenuvi <chenuvi@outlook.com>
2020-11-12 13:57:41 +0800

    模块化优化与打包完成

---
+++ src/main.js
@@ -0,0 +1,53 @@
+import {styleString} from './styleStr.js'
+console.log(styleString)
+let string = styleString.str
+
+let n = 1
+demo.innerText = string.substr(0, n)
+demo2.innerHTML = string.substr(0, n)
+
+let time = 100
+const run = () => {
+    n += 1
+    if (n > string.length) {
+        pause()
+        return
+    }
+    demo.innerText = string.substr(0, n)
+    demo2.innerHTML = string.substr(0, n)
+    demo.scrollTop = demo.scrollHeight
+}
+const play = ()=>{
+    return setInterval(run,time)
+}
+const pause = ()=>{
+    window.clearInterval(id)
+}
+let id = play()
+
+btnPause.onclick = ()=>{
+    pause()
+}
+btnPlay.onclick = ()=>{
+    pause()
+    time = 100
+    id = play()
+}
+btnSlow.onclick = ()=>{
+    pause()
+    time = 300
+    id = play()
+    console.log('slow')
+}
+btnNormal.onclick = ()=>{
+    pause()
+    time = 100
+    id = play()
+    console.log('quick')
+}
+btnFast.onclick = ()=>{
+    pause()
+    time = 0
+    id = play()
+    console.log('fast')
+}
